6d5bb9551e76ce41cc3700d72b9047dbdb869825
TheLimeGlass <seantgrover@gmail.com>
2024-03-30 19:27:53 -0600

    Clean up imports

--- src/main/java/io/github/syst3ms/skriptparser/registration/DefaultRegistration.java
+++ src/main/java/io/github/syst3ms/skriptparser/registration/DefaultRegistration.java
@@ -5,8 +5,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import io.github.syst3ms.skriptparser.Parser;
-import io.github.syst3ms.skriptparser.lang.VariableString;
-import io.github.syst3ms.skriptparser.parsing.ParserState;
 import io.github.syst3ms.skriptparser.structures.functions.FunctionParameter;
 import io.github.syst3ms.skriptparser.types.Type;
 import io.github.syst3ms.skriptparser.types.TypeManager;
